2cb1db953814f77ab8a8003f44aeecdd4aa93ffd
EvgenijMerchansky <evgenij.merchansky@gmail.com>
2019-07-13 17:21:19 +0300

    fix: add build directory

---
+++ build/precache-manifest.1bcdb0de963de57534ed4ddf0de4ed53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555043ffce18e7e0d22e49621313736c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df9f676470018c394010",
+    "url": "/static/css/2.e93aaed6.chunk.css"
+  },
+  {
+    "revision": "d442a916a1332b500e57",
+    "url": "/static/css/main.20260195.chunk.css"
+  },
+  {
+    "revision": "df9f676470018c394010",
+    "url": "/static/js/2.f95925a5.chunk.js"
+  },
+  {
+    "revision": "d442a916a1332b500e57",
+    "url": "/static/js/main.bca6b987.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d2b6844b6b617bd976371b2a64bb9ba",
+    "url": "/static/media/admin-panel-background-images-5.8d2b6844.jpg"
+  }
+]);
