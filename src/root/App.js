c6553f6cf4813ef1b4ce6d87bde44a3e3aaaba6e
EvgenijMerchansky <evgenij.merchansky@gmail.com>
2019-07-15 12:47:19 +0300

    fix: remove unnecessary logging

--- src/root/App.js
+++ src/root/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Login from '../Pages/SignIn/index';
 import Dashboard from '../Pages/Dashboard/index';
 import './App.css';
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 const bgImage = require("../assets/admin-panel-background-images-5.jpg");
 
 function App() {
-  console.log(browserHistory, 'browserHistory');
-  
   return (
     <div
       className="App"
